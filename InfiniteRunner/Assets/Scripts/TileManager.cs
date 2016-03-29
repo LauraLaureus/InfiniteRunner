e7a3856d92cc9a23986c77d506dabd64f2c76579
Laura <l4depende@gmail.com>
2016-03-29 17:17:41 +0100

    TileME

---
+++ InfiniteRunner/Assets/Scripts/TileManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+	public GameObject prefab;
+	public GameObject current;
+	
+	void OnEnable(){
+		TileScript.playerExited += spawnOne;
+	}
+
+	void OnDisenable(){
+		TileScript.playerExited -= spawnOne;
+	}
+
+	protected void spawnOne(){
+		current = (GameObject) Instantiate (prefab, current.transform.GetChild ((int)Random.Range(0,2)).position, Quaternion.identity);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		SpawnTile ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SpawnTile(){
+		for(int i  = 0; i < 10; i++)
+			current = (GameObject) Instantiate (prefab, current.transform.GetChild ((int)Random.Range(0,2)).position, Quaternion.identity);
+	}
+}
---
+++ InfiniteRunner/Assets/Scripts/TileScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileScript : MonoBehaviour {
+
+	public delegate void OnTriggerExitEvent();
+	public static event OnTriggerExitEvent playerExited;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Player") {
+			if(playerExited != null)
+				playerExited();
+			Destroy(this.gameObject);
+		}
+	}
+}
